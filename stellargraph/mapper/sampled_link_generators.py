e861b865aae48018977ecef0e530b4f865154f59
Kevin Zhang <bwboy668@hotmail.com>
2021-09-20 19:05:00 +1000

    Update sampled_link_generators.py

--- stellargraph/mapper/sampled_link_generators.py
+++ stellargraph/mapper/sampled_link_generators.py
@@ -190,9 +190,9 @@ class BatchedLinkGenerator(Generator):
                 "Please pass a list of samples or a UnsupervisedSampler object."
             )
 
-    @classmethod
-    def run(cls, b):
-        return cls.GG.node_ids_to_ilocs(b)
+    @staticmethod
+    def run(b):
+        return GG.node_ids_to_ilocs(b)
         
     def flow_from_dataframe(self, link_targets, shuffle=False):
         """
