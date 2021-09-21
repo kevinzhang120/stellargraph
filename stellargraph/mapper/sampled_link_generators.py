6147d7584e4951fa44abf45703b2fdd82c6d303d
Kevin Zhang <bwboy668@hotmail.com>
2021-09-21 20:40:03 +1000

    Update sampled_link_generators.py

--- stellargraph/mapper/sampled_link_generators.py
+++ stellargraph/mapper/sampled_link_generators.py
@@ -169,12 +169,12 @@ class BatchedLinkGenerator(Generator):
 
    #         link_ids = [self.graph.node_ids_to_ilocs(ids) for ids in link_ids]
             
-            p = Pool(mp.cpu_count())
+  #          p = Pool(mp.cpu_count())
     
-            link_ids = p.map(self.graph.node_ids_to_ilocs, link_ids)
+  #          link_ids = p.map(self.graph.node_ids_to_ilocs, link_ids)
     
-  #          os.system("taskset -p 0xff %d" % os.getpid())          
-  #          link_ids = Parallel(n_jobs=2*mp.cpu_count(), prefer="threads")(delayed(self.graph.node_ids_to_ilocs)(ids) for ids in link_ids)    
+            os.system("taskset -p 0xff %d" % os.getpid())          
+            link_ids = Parallel(n_jobs=2*mp.cpu_count(), prefer="threads")(delayed(self.graph.node_ids_to_ilocs)(ids) for ids in link_ids)    
                 
  #           GG=self.graph
             
